72a6e8d23203390bcd2db8620d9af884049d84d3
nahnah <nandanrmenon@gmail.com>
2021-01-22 15:05:56 +0530

    Revert "Settings: Show only the Build Number"
    This reverts commit 8bde72b60061ff8dbe027e6e9349e50e97e9cf39.

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -78,7 +78,7 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.ID);
+        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
     }
 
     @Override
